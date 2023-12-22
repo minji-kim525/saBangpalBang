ff223f3a62915e8d21af6e5af11e36c4e87cfbf2
kimhyeyeon59 <tldnjs0509@naver.com>
2023-12-22 14:32:07 +0900

    test

---
+++ pushtest/src/main/java/user/crawling/PushtestApplication.java
@@ -0,0 +1,13 @@
+package user.crawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushtestApplication.class, args);
+	}
+
+}
---
+++ pushtest/src/test/java/user/crawling/PushtestApplicationTests.java
@@ -0,0 +1,13 @@
+package user.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
