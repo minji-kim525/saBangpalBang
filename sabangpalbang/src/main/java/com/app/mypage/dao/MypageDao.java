65adbd68738b11de41102e3b62b5e57d48c68019
minji-kim525 <minjik0525@gmail.com>
2023-12-30 17:55:35 +0900

    방업로드 추가

--- sabangpalbang/src/main/java/com/app/mypage/dao/MypageDao.java
+++ sabangpalbang/src/main/java/com/app/mypage/dao/MypageDao.java
@@ -1,5 +1,25 @@
 package com.app.mypage.dao;
 
-public interface MypageDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.app.dto.ConfirmDTO;
+import com.app.dto.ImagesDTO;
+import com.app.mypage.dto.UploadRequestDto;
+
+@Mapper
+public interface MypageDao {
+	
+
+	int insertRoomInfo(UploadRequestDto dto);
+	
+	
+	int insertFile(@Param("list")List<ImagesDTO>list,@Param("id")int id);
+
+	@Insert("insert into confirm(confirm_check,ps_service_type,property_service_id)"+
+	"values(#{confirm_check},#{ps_service_type},#{property_service_id})")
+	int insertConfirm(ConfirmDTO confirmDto);
 }
