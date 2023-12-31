8b6174e73045b8b94565115e729907419e2ab916
minji-kim525 <minjik0525@gmail.com>
2024-01-01 02:56:26 +0900

    search xml 에러수정

--- sabangpalbang/src/main/java/com/app/property/dao/searchAllDAO.java
+++ sabangpalbang/src/main/java/com/app/property/dao/searchAllDAO.java
@@ -1,10 +1,13 @@
 package com.app.property.dao;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.app.dto.PropertyDTO;
 
+@Mapper
 public interface searchAllDAO {
 	
 	List<PropertyDTO> searchAll(@Param("keyword") String keyword);
