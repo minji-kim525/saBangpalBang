c71b0acddbd99502e4776d1d3f4d45485992465b
su <tkdgur4574@naver.com>
2024-01-25 17:17:44 +0900

    DB연결

--- sabangpalbang/src/main/java/com/app/SabangpalbangApplication.java
+++ sabangpalbang/src/main/java/com/app/SabangpalbangApplication.java
@@ -2,11 +2,18 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
-public class SabangpalbangApplication {
+public class SabangpalbangApplication extends SpringBootServletInitializer {
+	
+	@Override 
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
+	      return application.sources(SabangpalbangApplication.class);
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SabangpalbangApplication.class, args);
